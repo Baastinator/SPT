dceeb6811fd5d86ad33d7117f618820106dced89
root <root@seb-kring.com>
2023-06-03 06:40:23 +0200

    bruh

---
+++ run.sh
@@ -0,0 +1,1 @@
+ng serve --disable-host-check --port 4200 --host 0.0.0.0 --watch
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'SpellpointTracker';
+export class AppComponent implements OnInit {
+  public levelPoints: number[] = [
+      2, 3, 5, 6, 7, 9, 10, 11, 13
+  ];
+
+  public fullCaster: number[][] = [
+      [2],
+      [3],
+      [4, 2],
+      [4, 3],
+      [4, 3, 2],
+      [4, 3, 3],
+      [4, 3, 3, 1],
+      [4, 3, 3, 2],
+      [4, 3, 3, 3, 1],
+      [4, 3, 3, 3, 2],
+      [4, 3, 3, 3, 2, 1],
+      [4, 3, 3, 3, 2, 1],
+      [4, 3, 3, 3, 2, 1, 1],
+      [4, 3, 3, 3, 2, 1, 1],
+      [4, 3, 3, 3, 2, 1, 1, 1],
+      [4, 3, 3, 3, 2, 1, 1, 1],
+      [4, 3, 3, 3, 2, 1, 1, 1, 1],
+      [4, 3, 3, 3, 3, 1, 1, 1, 1],
+      [4, 3, 3, 3, 3, 2, 1, 1, 1],
+      [4, 3, 3, 3, 3, 2, 2, 1, 1],
+  ]
+
+  public level = 1
+
+  public max = 0;
+  public current = 0;
+
+  public title = 'SpellpointTracker';
+
+  public ngOnInit(): void {
+    this.levelReset()
+  }
+
+  public cast(n: number): void {
+    this.current -= this.levelPoints[n]
+  }
+
+  public reset(): void {
+    this.current = this.max;
+  }
+
+  public levelReset(): void {
+    const spellSlots = this.fullCaster[this.level-1].slice();
+    this.max = 0
+    spellSlots.forEach((v: number, i: number) => {
+      this.max += v * this.levelPoints[i];
+    })
+    this.reset()
+
+  }
 }
